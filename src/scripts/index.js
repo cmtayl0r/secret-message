0a0afc81732b1929cfc2f46495cf0ea5f026ead2
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-03-16 09:52:58 +0100

    add message and link forms

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,29 @@
 // -----------------------------------------------------------------------------
 // IMPORTS
 // -----------------------------------------------------------------------------
+
+import 'materialize-css/dist/css/materialize.min.css';
+
+// -----------------------------------------------------------------------------
+// DOM ELEMENTS
+// -----------------------------------------------------------------------------
+
+const formMsg = document.querySelector('#message-form');
+const formLink = document.querySelector('#link-form');
+const inputMsg = document.querySelector('#message-input');
+const inputLink = document.querySelector('#link-input');
+
+// -----------------------------------------------------------------------------
+// FORM SUBMISSION
+// -----------------------------------------------------------------------------
+
+formMsg.addEventListener('submit', e => {
+    e.preventDefault(); // Prevent the default form submission
+
+    formMsg.classList.add('hide'); // Hide the message form
+    formLink.classList.remove('hide'); // Show the link form
+
+    const encrypted = btoa(inputMsg.value); // Encrypt the message
+    inputLink.value = `${window.location}#${encrypted}`; // Set the input value to the encrypted
+    inputLink.select(); // Select the input value
+});
