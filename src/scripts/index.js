c49f9f7d054c3adaae296f7b90bd387093d78b6a
cmtayl0r <134958907+cmtayl0r@users.noreply.github.com>
2024-03-16 10:12:27 +0100

    add message and link forms

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,6 +12,7 @@ const formMsg = document.querySelector('#message-form');
 const formLink = document.querySelector('#link-form');
 const inputMsg = document.querySelector('#message-input');
 const inputLink = document.querySelector('#link-input');
+const messageShow = document.querySelector('#message-show');
 
 // -----------------------------------------------------------------------------
 // FORM SUBMISSION
@@ -27,3 +28,18 @@ formMsg.addEventListener('submit', e => {
     inputLink.value = `${window.location}#${encrypted}`; // Set the input value to the encrypted
     inputLink.select(); // Select the input value
 });
+
+// -----------------------------------------------------------------------------
+// MESSAGE SHOW
+// -----------------------------------------------------------------------------
+// Check if there is a hash in the URL
+const { hash } = window.location;
+const message = atob(hash.replace('#', '')); // Decrypt the message
+
+if (message) {
+    formMsg.classList.add('hide'); // Hide the message form
+    formLink.classList.add('hide'); // Hide the link form
+    messageShow.classList.remove('hide'); // Show the message
+
+    messageShow.querySelector('h2').innerHTML = message; // Set the message
+}
